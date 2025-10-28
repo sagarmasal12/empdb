f210cd98b6274eb81fcb7e8d3d1034363d0d60e1
sagarmasal12 <sagarmasal12@gmail.com>
2025-10-28 15:09:48 +0530

    Route

--- routes/empRoute.js
+++ routes/empRoute.js
@@ -1,10 +1,12 @@
 const express = require("express")
-const { getEmp, deleteEmp } = require("../controller/empController")
+const { getEmp, deleteEmp, createEmp } = require("../controller/empController")
 
 const router = express.Router()
 
 
 router.get("/getall",getEmp);
 router.delete('/delete/:emplId',deleteEmp)
+
+router.post('/create',createEmp);
 
 module.exports = router;
