fffacc64f324bf82bf59b99649e3a27af8fb6a4c
sagarmasal12 <sagarmasal12@gmail.com>
2025-10-29 10:09:51 +0530

    Add updateEmp function to handle employee updates in empController

--- controller/empController.js
+++ controller/empController.js
@@ -1,3 +1,4 @@
+
 const { sql, poolPromise } = require("../config/db.js");
 const{db} = require("../config/db.js")
 
@@ -109,4 +110,46 @@ const createEmp = async (req,res)=>{
 
     }
 }
-module.exports = {getEmp,deleteEmp,createEmp}
+
+const updateEmp = async (req,res)=>{
+
+    try{
+        const {emp_id} = req.params;
+        const {first_name,last_name,email, salary,department} = req.body;
+
+        if(!emp_id){
+            return res.status(400).send({
+                success:'false',
+                message:"Pls Provide emp_id or Emp Id not Provided"
+            })
+        }
+        const pool = await poolPromise;
+        const result = await pool
+        .request()
+        .input("emp_id",sql.Int,emp_id)
+        .input("first_name",sql.VarChar,first_name)
+        .input("last_name",sql.VarChar,last_name)
+        .input("email",sql.VarChar,email)
+        .input("salary",sql.Decimal(10,2),salary)
+        .input("department",sql.VarChar,department)
+        .query("UPDATE Employees SET first_name = @first_name, last_name = @last_name,email=@email,salary = @salary,department=@department WHERE emp_Id=@emp_Id");
+
+        if(result.rowsAffected[0]===0){
+            return res.status(404).send({
+                success:'false',
+                message:'Data Not Found'
+            })
+        }
+        res.status(200).send({
+            success:'true',
+            message:'Employee Updated Successfully'
+        })
+    }catch(error){
+        res.status(500).send({
+            success:'false',
+            message:'Error Updating Employee..',
+            error:error.message,
+        })
+    }
+}
+module.exports = {getEmp,deleteEmp,createEmp,updateEmp}
